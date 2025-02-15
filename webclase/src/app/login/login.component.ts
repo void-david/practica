5aa713146391a4ed7abbe4034d90276971f4784f
void david <void9david@gmail.com>
2025-02-14 18:08:41 -0600

    routes working

---
+++ webclase/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username === 'admin' && this.password === 'admin') {
+      this.router.navigate(['/']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+}
