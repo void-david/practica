8108920e4f63146f4aa8ac1f0843ae9b849db0e1
void david <void9david@gmail.com>
2025-02-19 19:11:59 -0600

    route protection implemented

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
