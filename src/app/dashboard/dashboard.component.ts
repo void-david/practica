653a519fb55bdb4c5659b5da6a5c3dc15c1f5002
void david <void9david@gmail.com>
2025-03-06 01:06:30 -0600

    pokemon api

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,25 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  userList: any [] = [];
+  user: any = {};
+  searchName: string = '';
+  errorMessage: string = '';
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.getUsers();
-  }
+  ngOnInit() {}
 
-  getUsers() {
-    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((result: any) => {
-      this.userList = result;
-    });
+  getUser() {
+    if (!this.searchName) {
+      this.errorMessage = 'Please enter a Pokémon name.';
+      return;
+    }
+
+    this.http.get(`https://pokeapi.co/api/v2/pokemon/${this.searchName.toLowerCase()}`).subscribe(
+      (result: any) => {
+        this.user = result;
+        this.errorMessage = '';
+      },
+      (error) => {
+        this.errorMessage = 'Pokémon not found. Please try again.';
+        this.user = {};
+      }
+    );
   }
 }
